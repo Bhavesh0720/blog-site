c5e272e37fdf92c762cb1c00b0a1b8b58bc67fa3
Bhavesh0720 <bhavesh0720@gmail.com>
2026-04-21 16:43:29 +0530

    added static folder inside blog_main dir

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -115,8 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
-    'static',
+    'blog_main/static',
 ]
 
