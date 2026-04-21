a8886c60a74c0650ea59be8c054974a701ca3304
Bhavesh0720 <bhavesh0720@gmail.com>
2026-04-21 19:50:16 +0530

    Displayed featured post dynamicaly

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from blogs.models import Category, Blog
 
 
 def home(request):
-    return render(request, 'home.html')
+    categories = Category.objects.all()
+    fp = Blog.objects.filter(is_featured=True).order_by('-updated_at')
+    con = {
+        'categories':categories,
+        'fp':fp,
+    }
+    return render(request, 'home.html', con)
