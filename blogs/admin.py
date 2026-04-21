ad8deeaadfb863c877fca4dbbd66069407ae685e
Bhavesh0720 <bhavesh0720@gmail.com>
2026-04-21 18:12:30 +0530

    alterd Admin panel

--- blogs/admin.py
+++ blogs/admin.py
@@ -7,6 +7,8 @@ class BlogAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('title',)}
     list_display = ('title', 'author', 'category', 'status', 'is_featured')
     search_fields = ('title', 'category__category_name', 'status')
+    list_editable = ('is_featured',)
+    list_filter = ('category__category_name', 'status')
 
 admin.site.register(Category)
 admin.site.register(Blog, BlogAdmin)
