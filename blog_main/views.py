b23d7efd7f0ba1beffa7b923c37964d7a68d5dc4
Bhavesh0720 <bhavesh0720@gmail.com>
2026-04-22 12:12:36 +0530

    Diplayed posts by category

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,12 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from blogs.models import Category, Blog
 
 
 def home(request):
-    categories = Category.objects.all()
-    fp = Blog.objects.filter(is_featured=True).order_by('-updated_at')
+    fp = Blog.objects.filter(is_featured=True, status='Published').order_by('-updated_at')
+    posts = Blog.objects.filter(is_featured=False, status='Published')
     con = {
-        'categories':categories,
         'fp':fp,
+        'posts':posts,
     }
     return render(request, 'home.html', con)
+
+
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Blog, Category
 
 # Create your views here.
+def post_by_category(request, pk):
+    posts = Blog.objects.filter(category=pk, status='Published')
+    category = get_object_or_404(Category, pk=pk)
+
+    # try:
+    #     category = Category.objects.get(pk=pk)
+    # except Category.DoesNotExist:
+    #     return redirect('home')      
+        
+    con = {
+        'posts':posts,
+        'category':category,
+    }
+    return render(request, 'post_by_category.html', con)
