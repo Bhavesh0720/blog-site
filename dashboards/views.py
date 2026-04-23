a2ab3ea2d677e4d4a0b25eed2c09f2da456e2e72
Bhavesh0720 <bhavesh0720@gmail.com>
2026-04-23 19:27:39 +0530

    diplayed post and add post functionality

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from blogs.models import Category, Blog
 from django.contrib.auth.decorators import login_required
-from .forms import CategoryForm
+from .forms import CategoryForm, PostForm
+from django.template.defaultfilters import slugify
+# from django.utils.text import slugify
 
 
 # Create your views here.
@@ -19,7 +21,7 @@ def dashboard(request):
 def categories(request):
     return render(request, 'dashboard/categories.html')
 
-
+@login_required(login_url='login')
 def add_category(request):
     if request.method == 'POST':
         form = CategoryForm(request.POST)
@@ -33,7 +35,7 @@ def add_category(request):
     }
     return render(request, 'dashboard/add_category.html', con)
 
-
+@login_required(login_url='login')
 def edit_category(request, pk):
     category = get_object_or_404(Category, pk=pk)
 
@@ -50,8 +52,51 @@ def edit_category(request, pk):
     }
     return render(request, 'dashboard/edit_category.html', con)
 
-
+@login_required(login_url='login')
 def delete_category(request, pk):
     category = get_object_or_404(Category, pk=pk)
     category.delete()
     return redirect('categories')
+
+@login_required(login_url='login')
+def posts(request):
+    posts = Blog.objects.all()
+    con = {
+        'posts':posts,
+    }
+    return render(request, 'dashboard/posts.html', con)
+
+
+def generate_slug(title):
+    base_slug = slugify(title)
+    slug = base_slug 
+    counter = 1
+
+    while Blog.objects.filter(slug=slug).exists():
+        slug = f'{base_slug}-{counter}'
+        counter += 1
+
+    return slug
+    
+@login_required(login_url='login')
+def add_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False) # temporary save so add data like author
+            post.author = request.user
+            # post.save()            
+            post.slug = generate_slug(post.title)
+            post.save()
+            return redirect('posts')
+    form = PostForm()
+    con = {
+        'form':form,
+    }
+    return render(request, 'dashboard/add_post.html', con)
+
+@login_required(login_url='login')
+def delete_post(request, pk):
+    post = get_object_or_404(Blog, pk=pk)
+    post.delete()
+    return redirect('posts')
