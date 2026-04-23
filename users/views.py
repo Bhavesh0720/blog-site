d694682d991969844d37148b03dded873519d1f4
Bhavesh0720 <bhavesh0720@gmail.com>
2026-04-23 09:25:53 +0530

    login functionality

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from .forms import RegistrationForm
+from .forms import RegistrationForm, LoginForm
+from django.contrib import auth
 
 # Create your views here.
 def register(request):
@@ -14,3 +15,29 @@ def register(request):
         'form':form,
     }
     return render(request, 'register.html', con)
+
+
+def login(request):
+    if request.method == 'POST':
+        form = LoginForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return redirect('home')
+            else:
+                return redirect('login')
+    else:
+        form = LoginForm()
+    con = {
+        'form':form,
+    }
+    return render(request, 'login.html', con)
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
